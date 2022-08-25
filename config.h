7f3d35e4fc6e2aedfe87643053546dc1e153680e
James Eidson <eidsonje@icloud.com>
2022-08-24 23:43:48 -0600

    dwm: bar styling

--- config.h
+++ config.h
@@ -1,26 +1,75 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Misc Tamsyn:size=15" };
-static const char rofitheme[]       = "gruvbox-dark";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Misc Tamsyn:size=12" };
+static const char rofitheme[]       = "gruvbox-dark-hard";
+
+/* gruvbox colors */
+static const char dark0_hard[]      = "#1D2021";
+// static const char dark0[]           = "#282828";
+// static const char dark0_soft[]      = "#32302F";
+// static const char dark1[]           = "#3c3836";
+// static const char dark2[]           = "#504945";
+// static const char dark3[]           = "#665C54";
+// static const char dark4[]           = "#7C6F64";
+
+// static const char gray_245[]        = "#928374";
+// static const char gray_244[]        = "#928374";
+
+// static const char light0_hard[]     = "#F9F5D7";
+// static const char light0[]          = "#FBF1C7";
+// static const char light0_soft[]     = "#F2E5BC";
+static const char light1[]          = "#EBDBB2";
+// static const char light2[]          = "#D5C4A1";
+// static const char light3[]          = "#BDAE93";
+// static const char light4[]          = "#A89984";
+
+// static const char bright_red[]      = "#FB4934";
+// static const char bright_green[]    = "#B8BB26";
+// static const char bright_yellow[]   = "#FABD2F";
+// static const char bright_blue[]     = "#83A598";
+// static const char bright_purple[]   = "#D3869B";
+// static const char bright_aqua[]     = "#8EC07C";
+// static const char bright_orange[]   = "#FE8019";
+
+// static const char neutral_red[]     = "#CC241D";
+// static const char neutral_green[]   = "#98971A";
+// static const char neutral_yellow[]  = "#D79921";
+// static const char neutral_blue[]    = "#458588";
+// static const char neutral_purple[]  = "#B16286";
+// static const char neutral_aqua[]    = "#689D6A";
+// static const char neutral_orange[]  = "#D65D0E";
+
+// static const char faded_red[]       = "#9D0006";
+static const char faded_green[]     = "#79740E";
+//static const char faded_yellow[]    = "#B57614";
+//static const char faded_blue[]      = "#076678";
+//static const char faded_purple[]    = "#8F3F71";
+//static const char faded_aqua[]      = "#427B58";
+//static const char faded_orange[]    = "#AF3A03";
+
+/* custom colors */
+static const char forest_green[]    = "#302F17";
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*                  text           background      border                         */
+	[SchemeNorm]      = { light1,      dark0_hard,    forest_green  }, // unfocused window
+	[SchemeSel]       = { light1,      dark0_hard,    light1      }, // focused window 
+	[SchemeStatus]    = { light1,      dark0_hard,    "#000000"   }, // Statusbar right
+	[SchemeTagsSel]   = { light1,      forest_green,    "#000000"   }, // Tagbar left selected
+	[SchemeTagsNorm]  = { light1,      dark0_hard,  "#000000"   }, // Tagbar left unselected
+	[SchemeInfoSel]   = { light1,      forest_green,    "#000000"   }, // infobar middle selected
+	[SchemeInfoNorm]  = { light1,      dark0_hard,    "#000000"   }, // infobar middle unselected
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "", "", "﬏", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "[term]", "[web ]", "[code]", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -63,12 +112,14 @@ static const char *termcmd[]  = { "alacritty", NULL };
 static const char *dmenucmd[] = { "rofi", "-modi", "drun", "-show", "drun", "-theme", rofitheme, NULL };
 static const char *swapcmd[] = {"rofi", "-show", "window", "-theme", rofitheme, NULL };
 static const char *browsecmd[]  = { "firefox", NULL };
+static const char *editcmd[]  = { "code", NULL };
 
 static Key keys[] = {
 	/* modifier                     key              function        argument */
 	{ MODKEY,                       XK_Return,       spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_d,            spawn,          {.v = dmenucmd} },
 	{ MODKEY,                       XK_w,            spawn,          {.v = browsecmd} },
+	{ MODKEY,                       XK_c,            spawn,          {.v = editcmd} },
 	{ MODKEY,                       XK_b,            togglebar,      {0} },
 	{ MODKEY,                       XK_l,            focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_h,            focusstack,     {.i = -1 } },
