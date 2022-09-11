063cbd553f36e4df9cac88b7d233af0f7fd4afdd
James Eidson <eidsonje@icloud.com>
2022-09-11 17:46:13 -0600

    update statusbar

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=8" };
+static const char *fonts[]          = { "Misc Tamsyn:size=13", "JetBrainsMono Nerd Font:size=9" };
 
 /* colors */
 static const char dark0_hard[]      = "#1D2021";
