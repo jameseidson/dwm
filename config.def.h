47030946214e4de6180a5b90d8f217f3e7b0646b
jameseidson <eidsonje@icloud.com>
2023-06-08 13:30:05 -0600

    updated dwm config for helix + improved languages.toml

--- config.def.h
+++ config.def.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar            = 1;     /* 0 means no bar */
-static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11" };
+static const unsigned int borderpx       = 1;  /* border pixel of windows */
+static const int swallowfloating         = 0;  /* 1 means swallow floating windows by default */
+static const unsigned int snap           = 32; /* snap pixel */
+static const unsigned int systraypinning = 0;  /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft  = 0;  /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;  /* systray spacing */
+static const int systraypinningfailfirst = 1;  /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray             = 1;  /* 0 means no systray */
+static const int showbar                 = 1;  /* 0 means no bar */
+static const int topbar                  = 1;  /* 0 means bottom bar */
+static const char *fonts[]               = { "JetBrainsMono Nerd Font:size=11" };
 
 /* colors */
 static const char dark0_hard[]      = "#1D2021";
@@ -32,28 +32,28 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1 term", "2 code", "3 web", "4", "5", "6", "7", "8", "9 misc" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class                instance  title  tags mask  switchtotag  isfloating  isterminal  noswallow  monitor */
-	{ "Alacritty",          NULL,     NULL,  1,         1,           0,          1,          0,         -1 },
-	{ "Code",               NULL,     NULL,  1 << 1,    1,           0,          0,          0,         -1 },
-	{ "firefox",            NULL,     NULL,  1 << 2,    1,           0,          0,          0,         -1 },
-	{ "Mailspring",         NULL,     NULL,  1 << 3,    1,           0,          0,          0,         -1 },
-	{ "Bitwarden",          NULL,     NULL,  0,         0,           1,          0,          0,         -1 },
-	{ "Blueman-manager",    NULL,     NULL,  0,         0,           1,          0,          0,         -1 },
-	{ "Qemu-system-x86_64", NULL,     NULL,  0,         0,           0,          0,          1,         -1 }
+	/* class                instance  title     tags mask switchtotag  isfloating  isterminal  noswallow  monitor */
+	{ "Alacritty",          NULL,     NULL,     1,        1,           0,          1,          0,         -1 },
+	// { NULL,                 NULL,     "helix",  1 << 1,   1,           0,          0,          0,         -1 },
+	{ "firefox",            NULL,     NULL,     1 << 2,   1,           0,          0,          0,         -1 },
+	{ "Mailspring",         NULL,     NULL,     1 << 9,   1,           0,          0,          0,         -1 },
+	{ "Bitwarden",          NULL,     NULL,     0,        0,           1,          0,          0,         -1 },
+	{ "Blueman-manager",    NULL,     NULL,     0,        0,           1,          0,          0,         -1 },
+	{ "Qemu-system-x86_64", NULL,     NULL,     0,        0,           0,          0,          1,         -1 }
 };
 
 /* layout(s) */
-static const float mfact     = 0.60; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const float mfact        = 0.60; /* factor of master area size [0.05..0.95] */
+static const int nmaster        = 1;    /* number of clients in master area */
+static const int resizehints    = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -75,13 +75,13 @@ static const Layout layouts[] = {
 
 /* commands */
 // static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *termcmd[]   = { "alacritty", NULL };
-static const char *dmenucmd[]  = { "rofi", "-modi", "drun", "-show", "drun", NULL };
-static const char *swapcmd[]   = { "rofi", "-theme-str", "window {width: 30%;}", "-show", "window", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *dmenucmd[] = { "rofi", "-modi", "drun", "-show", "drun", NULL };
+static const char *swapcmd[]  = { "rofi", "-theme-str", "window {width: 30%;}", "-show", "window", NULL };
 
-static const char *web[]  = { "firefox", NULL }; 
-static const char *code[] = { "code", NULL };
-static const char *mail[] = { "mailspring", NULL };
+static const char *web[]    = { "firefox", NULL }; 
+// static const char *editor[] = { "alacritty", "--title", "helix", "-e", "hx", NULL };
+static const char *mail[]   = { "mailspring", NULL };
 
 static const char *powermenu[] = { "/home/james/config/scripts/powermenu", NULL};
 static const char *inc_vol[]   = {"pamixer", "--increase", "1", NULL};
@@ -92,8 +92,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return,       spawn,          {.v = termcmd} },
 	{ MODKEY,                       XK_d,            spawn,          {.v = dmenucmd} },
 	{ MODKEY,                       XK_w,            spawn,          {.v = web} },
-	{ MODKEY,                       XK_c,            spawn,          {.v = code} },
-	{ MODKEY,                       XK_m,            spawn,          {.v = mail} },
+	// { MODKEY,                       XK_c,            spawn,          {.v = editor} },
+	{ MODKEY,                       XK_F1,           spawn,          {.v = mail} },
 	{ MODKEY,                       XK_b,            togglebar,      {0} },
 	{ MODKEY,                       XK_l,            focusstack,     {.i = +1} },
 	{ MODKEY,                       XK_h,            focusstack,     {.i = -1} },
@@ -103,13 +103,12 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,        spawn,          {.v = dec_vol} },
 	{ MODKEY|ShiftMask,             XK_Return,       zoom,           {0} },
 	{ MODKEY,                       XK_Tab,          view,           {0} },
- 	{ MODKEY|ShiftMask,             XK_Tab,          spawn,          {.v = swapcmd} },
+ 	{ MODKEY,                       XK_grave,        spawn,          {.v = swapcmd} },
 	{ MODKEY,                       XK_q,            killclient,     {0} },
 	{ MODKEY,                       XK_t,            setlayout,      {.v = &layouts[0]} }, // tiled
-	{ MODKEY,                       XK_space,        setlayout,      {.v = &layouts[1]} }, // floating
-	{ MODKEY,                       XK_f,            setlayout,      {.v = &layouts[2]} }, // fullscreen ('monacle')
-	// { MODKEY,                       XK_space,        setlayout,      {0} },             // swap to last used layout
-	{ MODKEY|ShiftMask,             XK_space,        togglefloating, {0} },
+	{ MODKEY,                       XK_f,            setlayout,      {.v = &layouts[1]} }, // floating
+	{ MODKEY,                       XK_m,            setlayout,      {.v = &layouts[2]} }, // fullscreen ('monacle')
+	{ MODKEY,                       XK_space,        togglefloating, {0} },
 	{ MODKEY,                       XK_0,            view,           {.ui = ~0} },
 	{ MODKEY|ShiftMask,             XK_0,            tag,            {.ui = ~0} },
 	{ MODKEY,                       XK_bracketleft,  focusmon,       {.i = -1} },
